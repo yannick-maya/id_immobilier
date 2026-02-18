2a97f1849e254a8dc2d635f5b0dff71d88f371d0
YannickMaya <138124217+yannick-maya@users.noreply.github.com>
2026-02-18 22:23:12 +0000

    ingestion et le netoyage

--- pipeline/ingestion.py
+++ pipeline/ingestion.py
@@ -25,14 +25,14 @@ def ingest():
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     for source_name, filename in SOURCES.items():
-        print(f"\n📥 Ingestion : {source_name}")
+        print(f"\n Ingestion : {source_name}")
 
         path = os.path.join(SOURCES_DIR, filename)
 
         # Vérifie que le fichier existe avant de lire
         if not os.path.exists(path):
-            print(f"   ❌ Fichier introuvable : {path}")
-            print(f"   👉 Place le fichier '{filename}' dans : {SOURCES_DIR}")
+            print(f"    Fichier introuvable : {path}")
+            print(f"    Place le fichier '{filename}' dans : {SOURCES_DIR}")
             continue
 
         df = pd.read_excel(path, engine="openpyxl")
@@ -41,12 +41,12 @@ def ingest():
         output_path = os.path.join(OUTPUT_DIR, f"{source_name.lower()}.csv")
         df.to_csv(output_path, index=False, encoding="utf-8")
 
-        print(f"   ✅ {len(df)} lignes | {len(df.columns)} colonnes")
-        print(f"   💾 Sauvegardé : {output_path}")
+        print(f"   {len(df)} lignes | {len(df.columns)} colonnes")
+        print(f"   Sauvegardé : {output_path}")
 
 
 if __name__ == "__main__":
-    print(f"📂 Racine du projet : {BASE_DIR}")
-    print(f"📂 Dossier sources  : {SOURCES_DIR}")
+    print(f" Racine du projet : {BASE_DIR}")
+    print(f" Dossier sources  : {SOURCES_DIR}")
     ingest()
-    print("\n🎉 Ingestion terminée !")
+    print("\n Ingestion terminée !")
